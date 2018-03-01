92778d334d1d86a4724246119da198fc5d4a8d84
keziaeuodia <setiadikezia@gmail.com>
2018-03-01 11:21:59 +0800

    fifth commit Qs

--- src/generics/labs/Exercise_01.java
+++ src/generics/labs/Exercise_01.java
@@ -8,3 +8,49 @@ package generics.labs;
  *          dynamic usage.
  */
 
+class GenEx <T extends Number>{
+    T num, num2;
+
+    GenEx(T a, T b) {
+        num = a;
+        num2 = b;
+    }
+
+    public T getNum2() {
+        return num2;
+    }
+
+    public void setNum2(T num2) {
+        this.num2 = num2;
+    }
+
+    public T getNum() {
+        return num;
+    }
+
+    public void setNum(T num) {
+        this.num = num;
+    }
+}
+
+class Generics1 {
+    public static void main(String args[]) {
+
+
+        GenEx<Integer> obj1 = new GenEx<>(588, 698);
+        int i = (Integer) obj1.getNum();
+        int j = (Integer) obj1.getNum2();
+        System.out.println("value: " + i + ", " + j);
+
+        GenEx<Long> obj2 = new GenEx<>(4876593L, 849506829L);
+        long l = (Long) obj2.getNum();
+        long k = (Long) obj2.getNum2();
+        System.out.println("value: " + l + ", " + k);
+
+        GenEx rawObj = new GenEx(new Float(4.6), new Float(5.8));
+        float f = (Float) rawObj.getNum();
+        float g = (Float) rawObj.getNum2();
+        System.out.println("value: " + f + ", " + g);
+    }
+}
+
--- src/generics/labs/Exercise_02.java
+++ src/generics/labs/Exercise_02.java
@@ -1,7 +1,33 @@
 package generics.labs;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Generics Exercise 2:
  *
  *      Create a class with a generic method that takes in an array and prints out the value at a certain index.
+ *      when you have a normal class with a lot of class but there's one method that needs to be generic.
  */
+public class Exercise_02 {
+
+    public static < E > void printArray( E[] inputArray, int i ) {
+
+        System.out.println(inputArray[i]);
+    }
+
+    public static void main(String args[]) {
+
+        String[] stArray = { "Let ", "them ", "eat ", "cake!" };
+        Integer[] intArray = { 500, 400, 300, 200, 100 };
+
+
+        System.out.println("String Array: ");
+        printArray(stArray, 2);
+
+        System.out.println("Integer Array: ");
+        printArray(intArray, 4);
+
+    }
+
+}
